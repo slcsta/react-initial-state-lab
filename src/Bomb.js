c3ee33f8f6a3ec0dfa33ea0513bdc33e23620f68
Sarah <sarahlchavez@gmail.com>
2021-11-02 16:25:39 -0700

    commit tree 50321121679d41bba78d1f12445502bb933dc7a8

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,21 @@
 // your Bomb code here!
+import React, { Component } from 'react';
+
+export default class Bomb extends React.Component {
+    constructor(props) {
+        super();
+        this.state = {
+            secondsLeft: props.initialCount
+
+        }
+    }
+    
+    render() {
+        if(this.state.secondsLeft === 0) 
+        {
+            return ('Boom!')
+        }
+        return(`${this.state.secondsLeft} seconds left before I go boom!`)
+    }
+}
+
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,33 @@
 // your ImageSlider code here!
+import React, { Component } from 'react';
+
+export default class ImageSlider extends React.Component {
+    // we use the constructor to set the initial state
+    constructor() {
+        super();
+        this.state = {
+            currentSlideIndex: 0
+        }
+    }
+
+    increment = () => {
+
+        const newIndex = this.state.currentSlideIndex + 1;
+        this.setState({
+            currentSlideIndex: newIndex,
+        });
+    }; 
+    
+    render() {
+        return ( 
+        <div>
+            <h4>I am on slide {this.state.currentSlideIndex}</h4>
+        </div> 
+        )
+    }   
+}
+
+
+// initial state should have a property called currentSlideIndex that starts at 0
+// It should only render text 'I am on slide <Current_Slide>' where <Current_Slide> is the value of this.state.currentSlideIndex
+
